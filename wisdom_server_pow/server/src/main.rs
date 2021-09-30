a922b3e1aaaa475367952e8460fab461d5817842
Pawel Kornecki <->
2021-09-30 20:33:07 +0200

    Wisdom server: extract processing function

--- wisdom_server_pow/server/src/main.rs
+++ wisdom_server_pow/server/src/main.rs
@@ -1,35 +1,63 @@
-use tokio::io::{Error};
-use tokio_util::codec::{Framed, LinesCodec};
+use std::error::Error;
+use tokio::net::TcpStream;
 use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, LinesCodec};
 
-fn process(line: &str) {
-    println!("line: {:?}", line)
+struct Action<T> {
+    state: T,
+}
+
+struct Connected {}
+
+struct Challenge {}
+
+impl Action<Connected> {
+    fn new() -> Self {
+        Action {
+            state: Connected {},
+        }
+    }
+    fn process(&self, line: &str) {
+        println!("connected, line: {:?}", line)
+    }
+}
+impl Action<Challenge> {
+    fn process(line: &str) {
+        println!("challenge, line: {:?}", line)
+    }
+}
+
+async fn process<F>(lines: &mut Framed<TcpStream, LinesCodec>, action: F)
+where
+    F: FnOnce(&str),
+{
+    if let Some(result) = lines.next().await {
+        match result {
+            Ok(line) => action(&line),
+            Err(err) => eprintln!("error reading from stream: {:?}", err),
+        }
+    }
 }
 
 #[tokio::main]
-pub async fn main() -> Result<(), Error> {
+pub async fn main() -> Result<(), Box<dyn Error>> {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3962").await?;
 
     loop {
         // wait for an inbound socket
         let (socket, addr) = listener.accept().await?;
-        
+
         // handle connections
         tokio::spawn(async move {
             println!("connection established for {}", addr);
 
-            // decode the stream
-            let mut lines = Framed::new(socket, LinesCodec::new());
+            // decode the data
+            let mut data = Framed::new(socket, LinesCodec::new());
 
-            // iterate through the data
-            while let Some(result) = lines.next().await {
-                match result {
-                    Ok(line) => process(&line),
-                    Err(err) => eprintln!("socket closed with error: {:?}", err),
-                }
-            }
+            process(&mut data, |line| Action::<Connected>::new().process(line)).await;
+            process(&mut data, |line| Action::<Challenge>::process(line)).await;
 
-            println!("socket received FIN packet and closed connection");
+            println!("connection closed");
         });
     }
 }
