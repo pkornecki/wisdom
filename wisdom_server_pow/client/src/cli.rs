747530b357c969d35f3623327abec3b3d7393fcb
Pawel Kornecki <->
2021-10-02 23:23:22 +0200

    Wisdom server: add cmd line arguments to client

---
+++ wisdom_server_pow/client/src/cli.rs
@@ -0,0 +1,22 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "wisdom client",
+    about = "a client for retreiving words of wisdom from the wisdom server
+it implements a proof of work algorithm by solving a challenge sent by the server"
+)]
+pub struct CommandLineArgs {
+    /// ip address of the server
+    #[structopt(short, long)]
+    pub addr: Option<String>,
+
+    /// port to connect to
+    #[structopt(short, long)]
+    pub port: Option<u16>,
+
+    /// interactive mode
+    #[structopt(short, long)]
+    pub interactive: bool,
+}
+
--- wisdom_server_pow/client/src/main.rs
+++ wisdom_server_pow/client/src/main.rs
@@ -2,27 +2,44 @@ use tokio::io::BufReader;
 use tokio::net::TcpStream;
 use std::error::Error;
 use std::io;
+use structopt::StructOpt;
 use text_io::read;
+
+mod cli;
+
+use cli::CommandLineArgs;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    // establish a connection with the server
-    let stream = TcpStream::connect("127.0.0.1:3962").await?;
+    // get the command line arguments
+    let CommandLineArgs { addr, port, interactive } = CommandLineArgs::from_args();
+    let addr = addr.unwrap_or_else(|| "127.0.0.1".to_string());
+    let port = port.unwrap_or_else(|| 3962);
+
+    // establish a connection to the server
+    let stream = TcpStream::connect(format!("{}:{}", addr, port)).await?;
+
     // create an object for reading and writing from/to the stream
     let mut stream = BufReader::new(stream);
 
     loop {
-        print!("quote? (yes/no): ");
-        io::Write::flush(&mut io::stdout())?;
+        if interactive {
+            print!("quote? (yes/no): ");
+            io::Write::flush(&mut io::stdout())?;
 
-        let choice: String = read!("{}\n");
-        let choice = choice.trim().to_lowercase();
-        if choice != "y" && choice != "yes" {
-            break;
+            let choice: String = read!("{}\n");
+            let choice = choice.trim().to_lowercase();
+            if choice != "y" && choice != "yes" {
+                break;
+            }
         }
 
         let quote = client::get_quote(&mut stream).await?;
         println!("{}", quote);
+
+        if !interactive {
+            break;
+        }
     }
 
     Ok(())
